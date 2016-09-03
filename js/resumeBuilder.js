a02cc8af872dd75d3036a73be9e705201ee288f3
ashishgaigole2 <ashishgaigole2@gmail.com>
2016-09-03 11:23:22 +0530

    Variable

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,4 +2,5 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
  
- $("#main").append("Ashish");
+ var awesomeThoughts="I am Ashish and I am AWESOME" 
+ console.log(awesomeThoughts)
