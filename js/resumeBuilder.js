19a7f5a57ae63fde86bb724a04d269562189c265
ashishgaigole2 <ashishgaigole2@gmail.com>
2016-09-03 11:52:25 +0530

    var

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -7,3 +7,9 @@ This is empty on purpose! Your code to build the resume will go here.
  var funThoughts = awesomeThoughts.replace("AWESOME","FUN");
  $("#main").append(funThoughts);
  console.log(funThoughts);
+ var formatedName = HTMLheaderName.replace("%data%",name);
+ var role = "Web Developer";
+ var formatedRole = HTMLheaderRole.replace("%data%",role);
+ 
+ $("#header").prepend(formatedRole);
+ $("#header").prepend(formatedName);
