d0af315188dbf9ebfa34a6a699a0563cceb0d4bb
ashishgaigole2 <ashishgaigole2@gmail.com>
2016-09-03 11:42:38 +0530

    var

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -5,5 +5,5 @@ This is empty on purpose! Your code to build the resume will go here.
  var awesomeThoughts="I am Ashish and I am AWESOME" 
  console.log(awesomeThoughts)
  var funThoughts = awesomeThoughts.replace("AWESOME","FUN");
- $(#main).append(funThoughts);
+ $("#main").append(funThoughts);
  console.log(funThoughts);
